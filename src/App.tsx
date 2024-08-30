7ec0915bf7de1b4321480d22abc51c5979e0d7a3
李帅 <lishuai@troila.com>
2024-08-30 10:37:02 +0800

    起始的测试案例，仅供参考

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.less";
+import React, { lazy, Suspense, useState } from 'react'
+import smallImg from './assets/imgs/5kb.png'
+import bigImg from './assets/imgs/22kb.png'
+import Class from './components/Class'
+import './app.less'
+
+// prefetch
+const PreFetchDemo = lazy(() => import(
+  /* webpackChunkName: "PreFetchDemo" */
+  /*webpackPrefetch: true*/
+  '@/components/PreFetchDemo'
+))
+// preload
+const PreloadDemo = lazy(() => import(
+  /* webpackChunkName: "PreloadDemo" */
+  /*webpackPreload: true*/
+  '@/components/PreloadDemo'
+ ))
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
-}
+  const [ show, setShow ] = useState(false)
 
-export default App;
+  const onClickSSSSSSS = () => {
+    setShow(true)
+  }
+  return (
+    <>
+      <h2 onClick={onClickSSSSSSS}>展示</h2>
+      <Class />
+      {/* show为true时加载组件 */}
+      { show && (
+        <>
+          <img src={smallImg} alt="小于10kb的图片" />
+          <img src={bigImg} alt="大于于10kb的图片" />
+          <div className='smallImg'></div> {/* 小图片背景容器 */}
+          <div className='bigImg'></div> {/* 大图片背景容器 */}
+          <Suspense fallback={null}><PreloadDemo /></Suspense>
+          <Suspense fallback={null}><PreFetchDemo /></Suspense>
+        </>
+      ) }
+    </>
+  )
+}
+export default App
